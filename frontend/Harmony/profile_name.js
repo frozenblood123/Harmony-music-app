5091b75e3612b937053b2a105f0d59e6db8091f8
Harsh Malani <harsh13092001@gmail.com>
2021-12-09 12:46:04 +0530

    route fixes

--- frontend/Harmony/profile_name.js
+++ frontend/Harmony/profile_name.js
@@ -1,33 +1,41 @@
 
 document.writeln("<script type='text/javascript' src='https://unpkg.com/axios/dist/axios.min.js'></script>");
 
-var user_curr = {
-  "firstname": "veerabhadra",
-  "lastname" : "Khandelot",
-  "dob" : "12/03/2000",
-  "email" : "veerigcse@gmail.com",
-};
+
+
+
+function get_user_profile() {
+  var user_curr_ = {
+    "firstname": "Harsh",
+    "lastname": "Malani",
+    "email": "harsh13092001@gmail.com",
+    "dob": "13-09-2001",
+  };
 axios.post('http://localhost:90/get-user/', {
   "email": "harsh13092001@gmail.com"
 
 }, {})
   .then(response => {
-    user_response = response["data"];
-    user_curr.firstname = user_response.firstname;
-    user_curr.lastname = user_response.lastname;
 
-    user_curr.email = user_response.email;
-    user_curr.dob = user_response.dob;
-    console.log(user_curr);
+
+    user_response = response["data"];
+    user_curr_["firstname"] = user_response["firstname"];
+    user_curr_.lastname = user_response.lastname;
+
+    user_curr_.email = user_response.email;
+    user_curr_.dob = user_response.dob;
+
 
   })
   .catch(err1 => {
     console.log(err1);
   });
+  return user_curr_;
+}
 
-
-
-
+var user_curr;
+user_curr = get_user_profile();
+console.log(user_curr.firstname);
 var profile_name = document.querySelector("#user_fname").innerHTML;
 // var profile_page_name = document.querySelector("#user_fname2").innerHTML;
 // var email = document.querySelector("#email_field");
